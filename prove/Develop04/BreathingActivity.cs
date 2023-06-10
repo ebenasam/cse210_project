662da1c310bb22ed29f8e4e8c2f48cc02eeff2d1
ebenasam <ebenasam@outlook.com>
2023-06-10 18:37:24 +0000

    Develop04 completed

---
+++ prove/Develop04/BreathingActivity.cs
@@ -0,0 +1,20 @@
+using System;
+
+
+class BreathingActivity : Activity
+{
+    List<string> activityList = new List<string> { "Breath in", "Now breath out" };
+    
+    string description = "This activity will help you relax by walking you through breathing in and out slowly. Clear your mind and focus on your breathing.";
+    
+    public BreathingActivity(string _activityName) : base(_activityName)
+    {
+        SetDescription(description);
+        SetActivityList(activityList);
+    }
+
+    public List<string> getActivityList()
+    {
+        return activityList;
+    }
+}
